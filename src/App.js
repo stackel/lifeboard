61e7d1939dda6339c2431277f8588750a4530bff
Erik Stackelberg <stackelberg@splitstockholm.se>
2019-03-20 21:01:02 +0100

    Set up eslint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1 className="sans-serif tc f1"> Lifeboard </h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1 className="sans-serif tc f1">
+         Lifeboard
+      </h1>
+    </div>
+  );
 }
 
 export default App;
